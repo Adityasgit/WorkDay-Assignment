03eeeff73998d67b0d9a0703bf6743bc072e5419
Aditya <adityaaroraaditya12@gmail.com>
2024-05-04 15:16:48 +0530

    added browser router (newer version)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import JobListings from './components/JobListings.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<JobListings />}>
+      <Route path="*" element={<JobListings />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
 
-    <Router>
-      <Routes>
-        <Route path="/" element={<JobListings />} />
-      </Routes>
-    </Router>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
 
 
   );
